30fc9e6cd2df6f4c79c427ec924cfeaa133236e5
black loll <blackloll.dev@gmail.com>
2020-05-26 10:28:38 +0700

    u

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const index = express()
+const app = express()
 const port = process.env.PORT || 4000
-index.use(bodyParser.urlencoded({ extended: false }))
-index.use(bodyParser.json())
-index.post('/webhook', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    reply(reply_token)
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
     res.sendStatus(200)
 })
-index.listen(port)
-function reply(reply_token) {
+app.listen(port)
+function reply(reply_token, msg) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {oPr8fa22uagdHuLXLeq97LBTvYR9kApc+QIrYVUoWfMfN3epp4TZl7ZhO4XFw+fvK9Jm5cN6wbGGlZqeweO7RI9gMITwrZsztrdLjtDquck/dHEmZXrDwEqcXkCWO6i4pP7ZEOyr5rwLdBt/voeRtQdB04t89/1O/w1cDnyilFU=}'
@@ -20,12 +21,8 @@ function reply(reply_token) {
         replyToken: reply_token,
         messages: [{
             type: 'text',
-            text: 'Hello'
-        },
-            {
-                type: 'text',
-                text: 'How are you?'
-            }]
+            text: msg
+        }]
     })
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
