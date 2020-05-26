927284ff1ff1d73a6dfc2d96a982e0398178b784
black loll <blackloll.dev@gmail.com>
2020-05-26 09:25:18 +0700

    up

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.listen(port)
 function reply(reply_token) {
     let headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer {xxxxxxx}'
+        'Authorization': 'Bearer {oPr8fa22uagdHuLXLeq97LBTvYR9kApc+QIrYVUoWfMfN3epp4TZl7ZhO4XFw+fvK9Jm5cN6wbGGlZqeweO7RI9gMITwrZsztrdLjtDquck/dHEmZXrDwEqcXkCWO6i4pP7ZEOyr5rwLdBt/voeRtQdB04t89/1O/w1cDnyilFU=}'
     }
     let body = JSON.stringify({
         replyToken: reply_token,
